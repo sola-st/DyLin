cbdde669d8caf7aca2d4e43d9cbd29f38fb79bd9
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2023-09-22 16:28:10 +0200

    Fixed placing the reports

--- run_single_project.sh
+++ run_single_project.sh
@@ -1,3 +1,3 @@
 docker run --cpus="20" --memory="150g" -v /home/eghbalaz/DyLin/reports/:/Work/reports/ dylin_project $1 > log_$1.txt
-mv reports results/reports_$1
+mv reports project_results/reports_$1
 mkdir reports
