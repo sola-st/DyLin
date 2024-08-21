06bbb4da0ee9a2aaafa1b0ed3f432ef3d15b9cdb
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2024-08-21 13:57:50 +0200

    Added more tests for in-place sort

--- tests/inplace_sort/program.py
+++ tests/inplace_sort/program.py
@@ -1,19 +1,22 @@
-a = list(range(0, 32132))
+a = list(range(0, 32131))
 b = list(range(0, 32132))
-c = list(range(231, 321032))
-d = list(range(0, 32132))
-e = list(range(0, 32132))
-f = list(range(0, 32132))
+c = list(range(231, 321033))
+d = list(range(0, 32134))
+e = list(range(0, 32135))
+f = list(range(0, 32136))
+h = list(range(0, 32137))
 
 sorted(a)  # DyLin warn
 x = sorted(b)  # DyLin warn
 y = sorted(c)  # DyLin warn
+h = sorted(h, reverse=True)  # DyLin warn
 
 d.sort()
 z = sorted(e)
 e.append([])
 k = sorted(f)
 f
+h
 
 # TODO example which only works for dynamic analysis
 
