19b90ea1a942a96ecd25050ff8a7dbf0d9f45423
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2024-09-08 00:11:32 +0200

    Added more info in analysis

--- src/dylin/analyses/ItemInListAnalysis.py
+++ src/dylin/analyses/ItemInListAnalysis.py
@@ -21,7 +21,7 @@ class ItemInListAnalysis(BaseDyLinAnalysis):
                     iid,
                     dyn_ast,
                     "PC-05",
-                    f"Searching for an item ({left}) in a long list (length {len(right)}) is not efficient. Consider using a set.",
+                    f"Searching for an item ({left}) in a long list (length {len(right)}) is not efficient (done for {self.size_map[uid]}). Consider using a set.",
                 )
 
     def not_in(self, dyn_ast, iid, left, right, result):
