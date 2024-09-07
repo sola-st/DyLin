3026308c95621b699c9086a542fbc37a7122522a
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2024-09-07 22:17:59 +0200

    New version of item in list

--- src/dylin/analyses/ItemInListAnalysis.py
+++ src/dylin/analyses/ItemInListAnalysis.py
@@ -6,21 +6,23 @@ class ItemInListAnalysis(BaseDyLinAnalysis):
         super().__init__(**kwargs)
         self.analysis_name = "ItemInListAnalysis"
         self.threshold = 100
+        self.count = 5
+        self.size_map = {}
 
     def _in(self, dyn_ast, iid, left, right, result):
         if type(right) == list and len(right) > self.threshold:
-            self.add_finding(
-                iid,
-                dyn_ast,
-                "PC-05",
-                f"Searching for an item ({left}) in a long list (length {len(right)}) is not efficient. Consider using a set.",
-            )
+            uid = id(right)
+            if uid not in self.size_map:
+                self.size_map[uid] = len(right)
+            else:
+                self.size_map[uid] += len(right)
+            if self.size_map[uid] > self.threshold * self.count:
+                self.add_finding(
+                    iid,
+                    dyn_ast,
+                    "PC-05",
+                    f"Searching for an item ({left}) in a long list (length {len(right)}) is not efficient. Consider using a set.",
+                )
 
     def not_in(self, dyn_ast, iid, left, right, result):
-        if type(right) == list and len(right) > self.threshold:
-            self.add_finding(
-                iid,
-                dyn_ast,
-                "PC-05",
-                f"Searching for an item ({left}) in a long list (length {len(right)}) is not efficient. Consider using a set.",
-            )
+        self._in(dyn_ast, iid, left, right, result)
